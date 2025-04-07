f6ba9fe73074048617c9b0a5eada6fb2a7c878e3
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-07 15:09:31 +0200

    complete

--- pari-e-dispari/js/main.js
+++ pari-e-dispari/js/main.js
@@ -5,7 +5,29 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
-function generateRandomNumber(Max, min){
+function generateRandomNumber(max, min){
     const randomNumber = Math.floor(Math.random() * (max - min +1) + min);
     return randomNumber;
+}
+function sumEvenOrOdd(n1, n2){
+    let result = n1 + n2;
+    if (result % 2 === 0){
+        return "pari";
+    }else{
+        return "dispari";
+    }
+}
+const utentChoice = prompt("Scegli: pari o dispari?");
+console.log("your choice: ", utentChoice);
+
+const utentNumber = parseInt(prompt("Inserisci un mnumero da 1 a 5:"));
+console.log("your number: ", utentNumber);
+
+const computerNumber= generateRandomNumber(5, 1);
+console.log("computer number: ", computerNumber);
+
+if(sumEvenOrOdd(computerNumber, utentNumber) === utentChoice){
+    alert("you win");
+}else{
+    alert("you lose");
 }
